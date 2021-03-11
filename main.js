50aae95d9b72ff7eb8864e76a44580ad7ca61418
propgmachine <77746698+propgmachine@users.noreply.github.com>
2021-03-10 19:59:33 -0500

    Updated main.js

--- main.js
+++ main.js
@@ -13,17 +13,17 @@ function fizzy(num) {
   return num;
 }
 
-function fizzyAlt1(num) {
-  if (num & 3 === 0 && num % 5 === 0) {
-    return "FizzBuzz";
-  } else if (num % 3 === 0) {
-    return "Fizz";
-  } else if (num % 5 === 0) {
-    return "Buzz";
-  } else {
-    return num;
-  }
-}
+//unction fizzyAlt1(num) {
+//  if (num & 3 === 0 && num % 5 === 0) {
+//    return "FizzBuzz";
+//  } else if (num % 3 === 0) {
+//    return "Fizz";
+//  } else if (num % 5 === 0) {
+//    return "Buzz";
+//  } else {
+//    return num;
+//  }
+//}
 
 function fixTheLamp() {
   if (lampNotWorking) {
